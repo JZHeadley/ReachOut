2b7df4bdd748adaeb33010767860507aaf18085a
jzheadley <zephyrinkg@gmail.com>
2017-02-18 02:40:09 -0500

    prompts working

--- app/src/main/java/com/jzheadley/reachout/views/ThreeButtonView.java
+++ app/src/main/java/com/jzheadley/reachout/views/ThreeButtonView.java
@@ -1,8 +1,10 @@
 package com.jzheadley.reachout.views;
 
 import android.content.Context;
+import android.speech.tts.TextToSpeech;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -11,9 +13,11 @@ import com.jzheadley.reachout.R;
 
 public class ThreeButtonView extends LinearLayout {
     private EditText threeButtonEditText;
-    private Button threeButtonOne;
-    private Button threeButtonTwo;
-    private Button threeButtonThree;
+    private Button threeButtonPlayPrompt;
+    private Button threeButtonAddResponse;
+    private Button threeButtonPlayResponse;
+    private String promptText;
+    private TextToSpeech ttobj;
 
     public ThreeButtonView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -25,19 +29,34 @@ public class ThreeButtonView extends LinearLayout {
         initControl(context);
     }
 
+    public void setPromptText(String promptText) {
+        threeButtonPlayPrompt.setContentDescription(promptText);
+    }
+
     /**
      * Load component XML layout
      */
     private void initControl(Context context) {
+        ttobj = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+            }
+        });
         LayoutInflater inflater = (LayoutInflater)
             context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         inflater.inflate(R.layout.three_button_view, this, true);
-        threeButtonEditText = (EditText) findViewById(R.id.three_button_edit_text);
-        threeButtonOne = (Button) findViewById(R.id.three_button_play_prompt);
-        threeButtonTwo = (Button) findViewById(R.id.three_button_add_response);
-        threeButtonThree = (Button) findViewById(R.id.three_button_play_response);
+        threeButtonEditText = (EditText) findViewById(R.id.view_three_button_edit_text);
+        threeButtonPlayPrompt = (Button) findViewById(R.id.view_three_button_play_prompt);
+        threeButtonAddResponse = (Button) findViewById(R.id.view_three_button_add_response);
+        threeButtonPlayResponse = (Button) findViewById(R.id.view_three_button_play_response);
 
-
+        threeButtonPlayPrompt.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ttobj.speak(String.valueOf(v.getContentDescription()), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
     }
+
 }
