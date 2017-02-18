79061f30ec2ac457b4850f4a3d4b87723d7f53ff
SarmentoAj <Sarmentoaj@vcu.edu>
2017-02-18 06:19:04 -0500

    Austin did the Nessie Service for real this time

--- app/src/main/java/com/jzheadley/reachout/models/services/NessieService.java
+++ app/src/main/java/com/jzheadley/reachout/models/services/NessieService.java
@@ -1,49 +1,114 @@
 package com.jzheadley.reachout.models.services;
 
 
+import android.util.Log;
+
 import com.jzheadley.reachout.App;
 import com.jzheadley.reachout.R;
+import com.jzheadley.reachout.models.dataobjects.Borrower;
 import com.jzheadley.reachout.models.dataobjects.Proposal;
+import com.reimaginebanking.api.nessieandroidsdk.NessieError;
+import com.reimaginebanking.api.nessieandroidsdk.NessieResultsListener;
 import com.reimaginebanking.api.nessieandroidsdk.constants.TransactionMedium;
 import com.reimaginebanking.api.nessieandroidsdk.constants.TransactionType;
+import com.reimaginebanking.api.nessieandroidsdk.models.Account;
 import com.reimaginebanking.api.nessieandroidsdk.models.Transfer;
+import com.reimaginebanking.api.nessieandroidsdk.requestclients.AccountClient;
 import com.reimaginebanking.api.nessieandroidsdk.requestclients.NessieClient;
+import com.reimaginebanking.api.nessieandroidsdk.models.PostResponse;
+import com.reimaginebanking.api.nessieandroidsdk.requestservices.AccountService;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Random;
 
 public class NessieService {
+    private static final String TAG = "NessieService";
     private static NessieClient client = NessieClient.getInstance(App.get().getResources().getString(R.string.nessie_api_key));
     private String date = DateFormat.getDateTimeInstance().format(new Date());
 
+    public void checkFunds(Borrower borrower) { //check for funding
+        final ArrayList<Proposal>props = borrower.getProposals();
 
-    public void fundProposal(Proposal proposal, String lenderAccountNumber) { //check for funding
-        Transfer transfer;
-        if (proposal.getState() == 2) {
-            transfer = new Transfer(getTransferId(), date, "Completed", TransactionType.P2P, TransactionMedium.BALANCE, Integer.toString(lenderAccountNumber), Integer.toString(proposal.getAccountNumber()), proposal.getAmountBorrowed(), "Lender to proposal");
-            proposal.funded(lenderAccountNumber);
+        for(int i = 0; i < props.size(); i++)
+        {
+            final Proposal proposal = props.get(i);
+            client.ACCOUNT.getAccount(proposal.getAccountNumber(), new NessieResultsListener() {
+                @Override
+                public void onSuccess(Object result) {
+                    Account account = (Account) result;
+
+                    if(account.getBalance() >= proposal.getAmountBorrowed())
+                    {
+
+                        client.TRANSFER.getTransfers(proposal.getAccountNumber(), new NessieResultsListener() {
+                            @Override
+                            public void onSuccess(Object result) {
+                                ArrayList<Transfer> transfers = (ArrayList<Transfer>) result;
+                                proposal.funded(transfers.get(0).getPayerId());
+                            }
+                            @Override
+                            public void onFailure(NessieError error){
+                                Log.d(TAG, "onFailure: "+error);
+                            }
+                        });
+                    }
+                }
+                @Override
+                public void onFailure(NessieError error){
+                    Log.d(TAG, "onFailure: "+error);
+                }
+
+            });
         }
     }
 
-    public void reciveFunds(Proposal proposal, int bankAccountNumber)
+    public void cashWithdrawl(Proposal proposal, int bankAccountNumber)
     {
         Transfer transfer;
         if(proposal.getState() == 3)
         {
-            transfer = new Transfer(getTransferId(), date, "Completed", TransactionType.P2P, TransactionMedium.BALANCE, Integer.toString(proposal.getAccountNumber()), Integer.toString(bankAccountNumber), proposal.getAmountBorrowed(), "Proposal to bank");
+            transfer = new Transfer(getTransferId(), date, "Completed", TransactionType.P2P, TransactionMedium.BALANCE, proposal.getAccountNumber(), Integer.toString(bankAccountNumber), proposal.getAmountBorrowed(), "Proposal to bank");
             proposal.cashWithdrawn();
         }
     }
 
-    public void repay(Proposal proposal, int bankAccountNumber)
-    { //check
-        Transfer transfer;
-        if(proposal.getState() == 4)
-        {
-            transfer = new Transfer(getTransferId(), date, "Completed", TransactionType.P2P, TransactionMedium.BALANCE, Integer.toString(bankAccountNumber), Integer.toString(proposal.getLenderAccountNumber()), proposal.getAmountToBeRepayed(), "Bank to lender");
-            proposal.cashRepaid();
-        }
+    public void repay(final Proposal proposal)
+    { //check balance of prop, if >= repayment amount send to lender
+        client.ACCOUNT.getAccount(proposal.getAccountNumber(), new NessieResultsListener() {
+            @Override
+            public void onSuccess(Object result) {
+                Account account = (Account) result;
+
+                if(account.getBalance() >= proposal.getAmountToBeRepayed())
+                {
+                        Transfer transfer = new Transfer.Builder()
+                            .medium(TransactionMedium.BALANCE)
+                            .payeeId(proposal.getLenderAccountNumber())
+                            .transactionDate(date)
+                            .amount(proposal.getAmountToBeRepayed())
+                            .description("Repay to lender")
+                            .build();
+
+                    client.TRANSFER.createTransfer(proposal.getAccountNumber(), transfer, new NessieResultsListener() {
+                        @Override
+                        public void onSuccess(Object result) {
+                            PostResponse<Transfer> response = (PostResponse<Transfer>) result;
+                        }
+                        @Override
+                        public void onFailure(NessieError error){
+                            Log.d(TAG, "onFailure: "+error);
+                        }
+                    });
+                    proposal.cashRepaid();
+                }
+            }
+            @Override
+            public void onFailure(NessieError error){
+                Log.d(TAG, "onFailure: "+error);
+            }
+        });
     }
 
     private String getTransferId(){
